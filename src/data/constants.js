65687f265fd31bd11e7fd56918a1bcfca0247609
UPHARISH24 <160720153+UPHARISH24@users.noreply.github.com>
2024-04-16 14:48:27 +0530

    file

---
+++ src/data/constants.js
@@ -0,0 +1,188 @@
+export const Bio = {
+  name: "HARISH UP",
+  roles: [
+    "FullStack Developer",
+    "Android Developer",
+  ],
+  description:
+    "Aim to be placed in a challenging organization that gives me scope to enhance my knowledge & skills in accordance with the latest trends and be a part of team that dynamically works towards growth of organization.",
+  github: "https://github.com/UPHARISH24",
+  resume:
+    "https://drive.google.com/file/d/1E8Rpg3KWom0hV1-hDOaQ4GZ2_xP9FXQC/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/harish-up-119734209/",
+};
+
+export const skills = [
+  {
+    title: "Skill",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Java",
+        image:
+          "icons8-java-144.svg",
+      },
+      {
+        name: "React Native",
+        image:
+          "react-native-1.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "SQl",
+        image:
+          "icons8-my-sql.svg",
+      },
+      {
+        name: "Node Js",
+        image:
+          "icons8-node-js-144.png",
+      },
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    role: "Flutter Developer",
+    company: "Eazythings Private Limeted",
+    date: "Jan 2024 - Feb 2024",
+    desc: "•	I worked as a Flutter developer, where my job was to create an app that would fetch data from a MQTT client and visualize it, as well as fetch data from the server. Using the things board source code, I developed the Eazythings app.",
+    skills: [
+      "Flutter",
+      "Android Studio",
+    ],
+  },
+  
+];
+
+export const education = [
+  {
+    id: 0,
+  //  img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Kiit.jpeg?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",
+    school: "Sri Venkateswaraa College of Technology",
+    date: "jun 2020 - fed 2024",
+    grade: "8.00 CGPA",
+    degree: "Electronics and Communication Engineering",
+  },
+  {
+    id: 1,
+  //  img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+    school: "St. Joseph's Higher Secondary School, Assisi Nagar, Madhavaram",
+    date: "Jun 2019 - Feb 2020",
+    grade: "60%",
+    degree: "Computer Science Group",
+  },
+    {
+      id: 2,
+   //   img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+      school: "St. Joseph's Higher Secondary School, Assisi Nagar, Madhavaram",
+      date: "Jun 2017 - Feb 2018",
+      grade: "80%",
+    },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Design and Development of Virtual Buttons using Unity to Control Home Appliances",
+    //date: "Jan 2024 - Feb 202",
+    description:
+      "Design and Development of Virtual Buttons using Unity to Control Home Appliances by integrating Augmented Reality (AR) with the Internet of Things (IoT).  We propose a system utilizing a Wi-Fi-based microcontroller (ESP32) that allows users to interact with virtual buttons on an AR interface to control real-world IoT devices. This approach eliminates the need for traditional remote controls and provides an intuitive user experience. The paper analyzes the system's performance, demonstrating successful control of multiple appliances at distances of up to 10 meters with minimal signal strength degradation.",
+    image:
+     "hq720.jpg",
+    tags: [
+      "Unity, ESP32",
+    ],
+  github: "https://github.com/UPHARISH24/DESIGN-AND-DEVELOPMENT-OF-VIRTUAL-BUTTONS-USING-UNITY-TO-CONTROL-HOME-APPLIANCES",
+  },
+  {
+    id: 9,
+    title: "Up Harish portfolio-website",
+    //date: "Jan 2024 - Feb 202",
+    description:
+      "To showcase my projects and social media links, have expansive personal personal portfolio website. Using React JS, 10 I developed this responsive personal portfolio website",
+    image:
+     "p.png",
+    tags: [
+      "react js",
+    ],
+  github: "https://github.com/UPHARISH24/portfolio-website",
+  },
+  {
+    id: 9,
+    title: "EazyThings Application",
+    date: "Jan 2024 - Feb 2024",
+    description:
+      "I've made the EasyThings app, which will display in the graph and other algorithm after retrieving the MQTT from the server.Flutter and the Thingboard source code helped me accomplish this",
+    image:
+     "Picture1.png",
+    tags: [
+      "Flutter",
+    ],
+  //  github: "https://github.com/rishavchanda/Trackify",
+  },
+  {
+    id: 9,
+    title: "BLUETOOTH_APP",
+    date: "Jan 2024 - Feb 2024",
+    description:
+      "With the aid of the flutter blue packages, I developed the Bluetooth app. Through this app, we will be able to operatecertain devices such as the Esp 32 and other Intemet of Things",
+    image:
+     "pngwing.png",
+    tags: [
+      "Flutter",
+    ],
+  github: "https://github.com/UPHARISH24/BLUETOOTH_APP",
+  },
+  {
+    id: 9,
+    title: "OnlineReservationSystem    ",
+    //date: "Jan 2024 - Feb 202",
+    description:
+      "This is the basic OnlineReservationSystem code that the Java application created.",
+    image:
+     "OnlineReservationSystem.png",
+    tags: [
+      "JAVA",
+    ],
+  github: "https://github.com/UPHARISH24/OnlineReservationSystem",
+  },
+  {
+    id: 9,
+    title: "JAVA_PROGRAM",
+    //date: "Jan 2024 - Feb 202",
+    description:
+      "Basic java programs",
+    image:
+     "java.png",
+    tags: [
+      "JAVA",
+    ],
+  github: "https://github.com/UPHARISH24/OnlineReservationSystem",
+  },
+];
+
